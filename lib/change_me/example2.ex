4168dc19d37466ad6677e00cf0b1254be4b20768
Josh Adams <joshuascottadams@gmail.com>
2021-09-17 22:14:30 -0400

    Add example2

---
+++ lib/change_me/example2.ex
@@ -0,0 +1,29 @@
+defmodule ChangeMe.Example2 do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  defmodule Foo do
+    use Ecto.Schema
+    import Ecto.Changeset
+
+    @primary_key false
+    embedded_schema do
+      field :foo, :string
+    end
+
+    def changeset(struct, attrs) do
+      struct
+      |> cast(attrs, [:foo])
+    end
+  end
+
+  embedded_schema do
+    embeds_one :foo, Foo 
+  end
+
+  def changeset(example, attrs) do
+    example
+    |> cast(attrs, [])
+    |> cast_embed(:foo)
+  end
+end
---
+++ test/change_me/example2_test.exs
@@ -0,0 +1,35 @@
+defmodule ChangeMe.Example22Test do
+  use ExUnit.Case
+
+  alias ChangeMe.Example2
+
+  # setup do
+  #   :ok
+  # end
+
+  test "casts valid data" do
+    example = %Example2{}
+
+    changeset = Example2.changeset(example, %{"foo" => %{"foo" => "bar"}})
+
+    assert changeset.valid?
+
+    struct = Ecto.Changeset.apply_changes(changeset)
+
+    assert struct == %Example2{foo: %Example2.Foo{foo: "bar"}, id: nil}
+  end
+
+  test "adds errors for invalid data" do
+    example = %Example2{}
+
+    changeset = Example2.changeset(example, %{"foo" => %{"foo" => 1}})
+
+    refute changeset.valid?
+
+    assert Ecto.Changeset.traverse_errors(changeset, & &1) == %{
+      foo: %{
+        foo: [{"is invalid", [type: :string, validation: :cast]}]
+      }
+    }
+  end
+end
